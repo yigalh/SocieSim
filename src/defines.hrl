5ef076aefa41b919dabe4788f2268badf9214a0c
Yigal <yigalh@gmail.com>
2016-02-17 19:38:01 +0200

    Added ticks and resource

--- src/defines.hrl
+++ src/defines.hrl
@@ -1,4 +1,8 @@
 -record(point,{x,y}).
 -record(need,{intensity,growRate,fulfillRate}).
--record(area,{p1=#point{},p2=#point}).
--record(humanState, {location=#point, needs, destination=#point, speed}).
+-record(area,{p1=#point{},p2=#point{}}).
+-record(humanState, {location=#point{}, needs, destination=#point{}, speed}).
+-record(resource, {width, height, occupation, availability, waiting}).
+
+-define(HUMAN_REFRESH_TICK, 0.1).
+-define(HUMAN_SEND_STATUS_TICK, 0.1).
